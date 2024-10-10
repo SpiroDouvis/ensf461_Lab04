124e4800f4bba1e04351d544f1f70f9efc576092
issygaudet <isabelle.gaudet@ucalgary.ca>
2024-10-10 03:11:32 +0000

    AHHHHHHHHHHHHHHH

--- lab04-template/scheduler.c
+++ lab04-template/scheduler.c
@@ -20,6 +20,10 @@ struct job
     int tickets; // number of tickets for lottery scheduling
     // TODO: add any other metadata you need to track here
     struct job *next;
+
+    //added by issy
+ 
+    int original_length;
 };
 
 // the workload list
@@ -34,6 +38,10 @@ void append_to(struct job **head_pointer, int arrival, int length, int tickets)
     new->arrival = arrival;
     new->length = length;
     new->tickets = tickets;
+
+    //ADDED
+
+    new->original_length = length;
 
     if (*head_pointer == NULL)
     {
@@ -193,14 +201,115 @@ void policy_SJF(int analysis)
 }
 
 
-void policy_STCF()
+void policy_STCF(int analysis)
 {
     printf("Execution trace with STCF:\n");
 
-    // TODO: implement STCF policy
+    int current_time=0;
+    int total_response_time=0;
+    int total_turnaround_time=0;
+    int total_wait_time=0;
+    int job_count=0;
 
-    printf("End of execution with STCF.\n");
+    int **results=NULL;
+    if(analysis){
+        results = (int**)malloc(sizeof(int*)*numofjobs);
+    }
+    //ARRAY OF PTRS
+
+    struct job *ptr=head;
+
+    while (head!=NULL){
+        //continues until all jobs are processed!!!
+        struct job *shortest_job=NULL;
+        struct job *prev=NULL;
+        struct job *old_shortest=NULL;
+        ptr=head;//reset ptr to head each iteration
+
+        
+        while(ptr!=NULL&&ptr->arrival<=current_time){
+            //FIND SHORTEST JOB
+            if(shortest_job==NULL || ptr->length<shortest_job->length){
+                //NEW SHORTEST JOB
+                shortest_job=ptr;
+                old_shortest=prev;
+            }
+            prev=ptr;
+            ptr=ptr->next;
+            //ITERATE... 
+    
+        }
+        if(shortest_job==NULL){
+            current_time++;
+            continue;
+        }
+        //IF NO JOBS ARRIVED - ADD AND RELOOP
+
+        if (shortest_job->length >0){
+            //IF SHORTEST JOB NOT DONE/HAS REMAINING TIME...
+            if(shortest_job->length == shortest_job->original_length){
+                total_response_time+=current_time-shortest_job->arrival;
+    
+                //ADD RESPONSE TIME OF SHORTEST JOB IF IT HASNT YET RUN- THIS IS FIRST TIME RUNNING
+            }
+            printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]\n",current_time,shortest_job->id,shortest_job->arrival,shortest_job->length);
+            shortest_job->length--;
+            //DECREMENT REMAINING LENGTH
+            current_time++;
+        }
+        if(shortest_job->length==0){
+            //IF JOB COMPLETE..
+            int turnaround_time = current_time - shortest_job->arrival;
+            int wait_time = turnaround_time - (shortest_job->original_length);
+
+            total_turnaround_time+=turnaround_time;
+            total_wait_time+=wait_time;
+            //ADD TURNAROUND TIME AND WAIT TIME
+            job_count++;
+            //ADD COMPLETED JOB
+
+            if(analysis){
+                results[shortest_job->id]=(int*)malloc(sizeof(int[4]));
+                results[shortest_job->id][0]=shortest_job->id;
+                results[shortest_job->id][1]=current_time-shortest_job->arrival;
+                results[shortest_job->id][2]=turnaround_time;
+                results[shortest_job->id][3]=wait_time;
+            }
+            if(old_shortest==NULL){
+                head=shortest_job->next;
+            }
+            else{
+                old_shortest->next=shortest_job->next;
+            }
+            free(shortest_job);
+            //FREE SHORTEST JOB AFTER ITS COMPLETED/CAN BE RMVD FRM JOB LIST
+        }
+    }
+
+    if (job_count>0){
+        //'if any jobs processed'
+        double avg_response_time=(double)total_response_time/job_count;
+        double avg_turnaround_time=(double)total_turnaround_time/job_count;
+        double avg_wait_time=(double)total_wait_time/job_count;
+
+        printf("Average -- Response: %.2f  Turnaround %.2f  Wait %.2f\n",avg_response_time,avg_turnaround_time,avg_wait_time);
+    }
+    if (analysis) {
+    printf("Begin analyzing STCF:\n");
+    if (results != NULL) {
+        for (int i = 0; i < numofjobs; i++) {
+            if (results[i] != NULL) {
+                printf("Job %d -- Response time: %d  Turnaround: %d  Wait: %d\n", results[i][0], results[i][1], results[i][2], results[i][3]);
+                free(results[i]);
+            }
+        }
+        free(results);
+    }
+    printf("End analyzing STCF.\n");
 }
+printf("End of execution with STCF.\n");
+}
+
 
 void policy_RR(int slice)
 {
